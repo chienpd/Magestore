784f1ff9c32b7a3edf951bb9e9886d632f1d9610
chienphamk58 <phamdinhchienk58@gmail.com>
2017-12-08 16:09:42 +0700

    Tao Module MultiVendor

---
+++ Magestore/Multivendor/registration.php
@@ -0,0 +1,12 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: pdc
+ * Date: 07/12/2017
+ * Time: 16:41
+ */
+\Magento\Framework\Component\ComponentRegistrar::register(
+    \Magento\Framework\Component\ComponentRegistrar::MODULE,
+    'Magestore_Multivendor',
+    __DIR__
+);
